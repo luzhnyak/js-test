5bc577d2cf715004f0b8df5f436a2a9c2c894afa
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2025-02-24 23:15:48 +0200

    script isValidWalk

--- start.js
+++ start.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+function isValidWalk(walk) {
+  const countN = walk.filter((direction) => direction === "n").length;
+  const countS = walk.filter((direction) => direction === "s").length;
+  const countE = walk.filter((direction) => direction === "e").length;
+  const countW = walk.filter((direction) => direction === "w").length;
+  return countN === countS && countE === countW && walk.length === 10;
+}
+
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"])); // Output: true
+console.log(
+  isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"])
+);
